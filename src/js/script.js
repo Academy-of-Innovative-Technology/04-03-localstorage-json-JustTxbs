97795066ca23c7dcf876268d0d97b5a06572e08d
Aiden <atoro26@aoiths.org>
2026-03-11 14:05:08 -0400

    changes

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,48 @@ var Database = {
 
 
 function loadDataSource() {
+	var localData = localStorage.getItem(Database.keyName);
+  var parsedData = JSON.parse(localData);
+  Database.data = parsedData
 
-	
-	displayData( Database.data );
+  displayData(Database.data);
 }
 
 
 
 function displayData(dataArray) {
+	var root = dataArray.response;
+	root.forEach(function(value,index) {
 
+		var html = `
+		<div class="card shadow-lg">
+          <img src="${value.image}" class="card-img-top" alt="${value.name.firstName} ${value.name.lastName}">
+          <div class="card-body">
+            <h5 class="card-title text-center mb-3">
+			${value.name.firstName} ${value.name.lastName}
+			</h5>
+            <p class="card-text text-center text-muted"></p>
 
+            <h6 class="fw-bold"></h6>
+            <ul class="list-unstyled">
+              <li></li>
+            </ul>
+
+            <h6 class="fw-bold"></h6>
+            <ul class="list-unstyled">
+              <li></li>
+            </ul>
+
+            <h6 class="fw-bold"></h6>
+            <ul class="list-inline">
+              <li class="list-inline-item badge bg-primary"></li>
+            </ul>
+          </div>
+        </div>`;
+
+		document.querySelector(".col").innerHTML += html;
+	});
+	
 }
 
 loadDataSource();
